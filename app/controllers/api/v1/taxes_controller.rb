f4f620a3b3867fa1dd19154e5f0a8e35b4d4143b
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2022-02-17 19:55:52 +0630

    refactoring api for tax work

--- app/controllers/api/v1/taxes_controller.rb
+++ app/controllers/api/v1/taxes_controller.rb
@@ -1,73 +1,27 @@
 class Api::V1::TaxesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :is_admins?
+  # before_action :authenticate_user!
+  # before_action :is_admins?
   skip_before_action :verify_authenticity_token
-  before_action :set_api_v1_tax, only: %i[ show edit update destroy ]
 
-  # GET /api/v1/taxes or /api/v1/taxes.json
   def index
-    @api_v1_taxes = Api::V1::Tax.all
+    @taxes = Api::V1::Tax.all
+    render json: @taxes
   end
 
-  # GET /api/v1/taxes/1 or /api/v1/taxes/1.json
-  def show
-  end
-
-  # GET /api/v1/taxes/new
-  def new
-    @api_v1_tax = Api::V1::Tax.new
-  end
-
-  # GET /api/v1/taxes/1/edit
-  def edit
-  end
-
-  # POST /api/v1/taxes or /api/v1/taxes.json
   def create
-    @api_v1_tax = Api::V1::Tax.new(api_v1_tax_params)
-
-    respond_to do |format|
-      if @api_v1_tax.save
-        format.html { redirect_to api_v1_tax_url(@api_v1_tax), notice: "Tax was successfully created." }
-        format.json { render :show, status: :created, location: @api_v1_tax }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @api_v1_tax.errors, status: :unprocessable_entity }
-      end
-    end
+    @tax = Api::V1::Tax.new(tax_params)
+    @tax.save
+    redirect_to api_v1_taxes_path
   end
 
-  # PATCH/PUT /api/v1/taxes/1 or /api/v1/taxes/1.json
-  def update
-    respond_to do |format|
-      if @api_v1_tax.update(api_v1_tax_params)
-        format.html { redirect_to api_v1_tax_url(@api_v1_tax), notice: "Tax was successfully updated." }
-        format.json { render :show, status: :ok, location: @api_v1_tax }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @api_v1_tax.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /api/v1/taxes/1 or /api/v1/taxes/1.json
-  def destroy
-    @api_v1_tax.destroy
-
-    respond_to do |format|
-      format.html { redirect_to api_v1_taxes_url, notice: "Tax was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def remove
+     @taxes = Api::V1::Tax.where(:tax_rate => params[:rate])
+     @taxes.delete_all
+     render json: { message: "#{params[:rate]} Tax removed" }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_v1_tax
-      @api_v1_tax = Api::V1::Tax.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def api_v1_tax_params
-      params.require(:api_v1_tax).permit(:tax_category, :tax_rate)
+    def tax_params
+      params.require(:tax).permit(:tax_rate, :tax_category)
     end
 end
